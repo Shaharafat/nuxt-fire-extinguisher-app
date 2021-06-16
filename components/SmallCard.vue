fe262656c37bb026b268afb39a1c6f30a73e09ea
Shah Arafat <shaharafhat@gmail.com>
2021-06-16 18:42:39 +0600

    basic landing page and products folder structure created

---
+++ components/SmallCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-container">
+    <img
+      :src="require(`@/assets/images/${card.image || 'fe1.jpg'}`)"
+      alt=""
+      class="image"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"],
+};
+</script>
+
+<style scoped>
+.card-container {
+  width: 24%;
+  height: 12.5rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  margin-bottom: 1.5rem;
+  cursor: pointer;
+}
+.image {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>this is products page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
